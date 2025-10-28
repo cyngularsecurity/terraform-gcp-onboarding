9078018df0dbb1ada981bd437c317f53275d9a46
dvirg-uc <dvirg@cyngularsecurity.com>
2025-10-28 17:28:06 +0200

    DEVOPS-949 - added plan for required iam permissions

--- main.tf
+++ main.tf
@@ -4,3 +4,30 @@
 #     prefix  = "PATH/TO/STATEFILE"
 #   }
 # }
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.45.2"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "5.45.2"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.7.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.13.1"
+    }
+  }
+}
